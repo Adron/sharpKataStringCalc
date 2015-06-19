0f0eef10b54e054a3af3293d09c1c6acfb0a5472
Adron Hall <adron.hall@sdc.com>
2015-06-19 14:35:38 -0700

    First commit of generated project.

---
+++ src/sharpKataStringCalc/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("sharpKataStringCalc")>]
+[<assembly: AssemblyProductAttribute("sharpKataStringCalc")>]
+[<assembly: AssemblyDescriptionAttribute("A string calculator code kata.")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
