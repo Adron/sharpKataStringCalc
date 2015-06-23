bcf604cd68263fafcf25f857506d7b4f31b75a69
Adron Hall <adron.hall@sdc.com>
2015-06-22 22:15:45 -0700

    The next few tests and implementations.

--- src/sharpKataStringCalc/Calculator.fs
+++ src/sharpKataStringCalc/Calculator.fs
@@ -3,5 +3,11 @@ namespace sharpKataStringCalc
 open System
 
 type Calculator() = 
-  member x.Add express = 
-    0
+  member x.Add expression = 
+    match expression with
+    | "" -> 0
+    | _ when expression.Contains " " -> 
+        let numbers = expression.Split [| ' ' |]
+        (Int32.Parse numbers.[0]) + (Int32.Parse numbers.[1])
+    | _ -> Int32.Parse expression
+  
--- tests/sharpKataStringCalc.Tests/Tests.fs
+++ tests/sharpKataStringCalc.Tests/Tests.fs
@@ -4,10 +4,18 @@ open System
 open sharpKataStringCalc
 open NUnit.Framework
 
+let calculator = Calculator()
+
 [<TestFixture>]
 type CalculatorTests() =
   [<Test>]
   member x.add_empty_string() =
-    let calculator = Calculator()
     let result = calculator.Add ""
     Assert.That(result, Is.EqualTo 0)
+
+  [<TestCase("1", Result = 1)>]
+  [<TestCase("2", Result = 2)>]
+  [<TestCase("1 2", Result = 3)>]
+  [<TestCase("2 3", Result = 5)>]
+  member x.Add_single_number_returns_that_number expression =
+    calculator.Add expression
