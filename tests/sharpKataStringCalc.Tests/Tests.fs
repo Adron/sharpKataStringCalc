bd2011ce8029587a54bafc3b00db843c50c4a90d
Adron Hall <adron.hall@sdc.com>
2015-06-22 22:54:32 -0700

    Final tests for the blog entry.

--- tests/sharpKataStringCalc.Tests/Tests.fs
+++ tests/sharpKataStringCalc.Tests/Tests.fs
@@ -9,13 +9,16 @@ let calculator = Calculator()
 [<TestFixture>]
 type CalculatorTests() =
   [<Test>]
-  member x.add_empty_string() =
+  member x.``should return zero if no string value is passed in.``() =
     let result = calculator.Add ""
     Assert.That(result, Is.EqualTo 0)
 
   [<TestCase("1", Result = 1)>]
   [<TestCase("2", Result = 2)>]
+  member x.``take one number and return that number`` expression =
+    calculator.Add expression
+
   [<TestCase("1 2", Result = 3)>]
   [<TestCase("2 3", Result = 5)>]
-  member x.Add_single_number_returns_that_number expression =
+  member x.``add single number to single number and return sum`` expression =
     calculator.Add expression
